f857fb816b18f9e4161220e9fc944e2055645942
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-03-15 22:19:46 +0200

    Проект 11 - Итерация по массиву объектов

---
+++ project-11 - map-object/src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+import Persons from './components/Persons';
+
+function App() {
+  return (
+    <div className="App">
+      <Persons />;
+    </div>
+  );
+}
+
+export default App;
---
+++ project-11 - map-object/src/components/Person.js
@@ -0,0 +1,17 @@
+import matchers from '@testing-library/jest-dom/matchers';
+
+function Person(props) {
+  console.log(props);
+  const { firstName, lastName, email, img } = props;
+  return (
+    <div className="card">
+      <img src={img} />
+      <h3>
+        {firstName} {lastName}
+      </h3>
+      <h4>{email}</h4>
+    </div>
+  );
+}
+
+export default Person;
