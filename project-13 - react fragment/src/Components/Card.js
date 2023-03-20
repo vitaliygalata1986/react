01df0f03a8688da44016cb02f25b27b55b8ee663
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-03-20 17:46:26 +0200

    use react fragment

---
+++ project-13 - react fragment/src/Components/Card.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const Card = () => {
+  return (
+    <React.Fragment>
+      <h1>Vitaliy</h1>
+      <h3>I am recording React cource</h3>
+      <button>Like!</button>
+    </React.Fragment>
+  );
+};
+
+export default Card;
