75575640749b49b50286b0b6940e632ca448bcb0
Vitaliy Galata <vitaliy.galata@tangramltd.com>
2023-08-21 12:30:55 +0300

    modified code

--- project-29-context-api/src/componets/ChangeUser.jsx
+++ project-29-context-api/src/componets/ChangeUser.jsx
@@ -2,16 +2,20 @@ import UserContext from '../context/UserContext';
 import { useContext } from 'react';
 
 function ChangeUser() {
-  const { userName: user, cnangeUserName: setUser } = useContext(UserContext); // хотим воспользоваться определенным контекстом
+  const { userName, cnangeUserName } = useContext(UserContext); // хотим воспользоваться определенным контекстом
 
   function handleFormSubmit(event) {
     event.preventDefault();
-    setUser(event.target.username.value);
+    cnangeUserName(event.target.username.value);
   }
 
   return (
     <>
-      <button onClick={() => setUser(user === 'Vitaliy' ? 'Elena' : 'Vitaliy')}>
+      <button
+        onClick={() =>
+          cnangeUserName(userName === 'Vitaliy' ? 'Elena' : 'Vitaliy')
+        }
+      >
         Change name
       </button>
       <form onSubmit={handleFormSubmit}>
